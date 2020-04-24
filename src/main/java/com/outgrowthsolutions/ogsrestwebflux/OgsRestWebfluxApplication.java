1752e1ca9ccc357b35e8209044701a2c0a419d4d
Arnold Ayaoan Jr <arnoldayaoanjr@yahoo.com>
2020-04-24 12:17:59 +0800

    Initial project commit

---
+++ src/main/java/com/outgrowthsolutions/ogsrestwebflux/OgsRestWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.outgrowthsolutions.ogsrestwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgsRestWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgsRestWebfluxApplication.class, args);
+    }
+
+}
